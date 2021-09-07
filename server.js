bb24d557c1bf033e370c7912681ffdee2fc9ef62
Avinash <avijadhav416@gmail.com>
2021-09-07 14:35:40 +0530

    procifile deleted

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use('/',Routes);
 
 
 
-const PORT= process.env.PORT ||8000;
+const PORT= process.env.PORT || 8000;
 
 
 const username=process.env.DB_USERNAME;
@@ -31,8 +31,8 @@ const URL=`mongodb+srv://${username}:${password}@ecommerceweb.xcihq.mongodb.net/
 
 Connection(process.env.MONGODB_URI || URL);
 
-if(process.env.NODE_ENV ==='production'){
-    app.use(express.static('client/build'))
+if(process.env.NODE_ENV ==="production"){
+    app.use(express.static('client/build'));
 }
 
 app.listen(PORT,()=>{
@@ -50,6 +50,6 @@ paytmParams['INDUSTRY_TYPE_ID'] = process.env.PAYTM_INDUSTRY_TYPE_ID,
 paytmParams['ORDER_ID'] = uuid(),
 paytmParams['CUST_ID'] = process.env.PAYTM_CUST_ID,
 paytmParams['TXN_AMOUNT'] = '100',
-paytmParams['CALLBACK_URL'] = 'http://localhost:8000/callback'
+paytmParams['CALLBACK_URL'] = '/callback'
 paytmParams['EMAIL'] = 'codeforinterview01@gmail.com'
 paytmParams['MOBILE_NO'] = '1234567852'
